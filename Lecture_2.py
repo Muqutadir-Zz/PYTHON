9e9dd1cb7f51c858ed5eb446d1ac8be8d9f2b836
Muqutadir-Zz <skmuqutadir26@gmail.com>
2026-01-02 01:12:40 +0530

    commit

---
+++ Lecture_2.py
@@ -0,0 +1,24 @@
+# STRINGS
+# str = "I LOVE PLAYING GAMES"
+# print(str[1:6])
+
+# str = "I LOVE PLAYING GAMES"
+# print(str.endswith("MES"))
+
+# str = "I LOVE PLAYING GAMES"
+# str = str.capitalize()
+# print(str)
+
+# str = "I LOVE PLAYING GAMES"
+# print(str.replace("O", "kk"))
+
+# str = "I LOVE PLAYING GAMES"
+# print(str.find("L"))
+
+# PRACTICE
+
+# str = "MUQUTADIR"
+# print(str[0:11])
+
+str = "YO THIS IS $$$ 1 THOUSAND"
+print(str.count("$"))
