0d97b7a3ccfa86ccbbd30c5a13d6e43419aeb344
Muqutadir-Zz <skmuqutadir26@gmail.com>
2026-01-02 22:17:00 +0530

    commit

---
+++ listandtuples.py
@@ -0,0 +1,48 @@
+# marks1 = 99.4
+# marks2 = 87.5
+# marks3 = 77.2 
+
+# marks = [94.4, 88.5, 95.2]
+# print(type(marks))
+# print(marks[0])
+# print(marks[2])
+
+# list = [6,1,2,3,5,4]
+# list.append(4)
+# print(list)
+
+# list = [6,1,2,3,5,4]
+# list.sort()
+# print(list)
+
+# list = [6,1,2,3,5,4]
+# list.sort(reverse=True)
+# print(list)
+
+# list = [6,1,2,3,5,4]
+# list.reverse()
+# print(list)
+
+# list = [6,1,2,3,5,4]
+#list.insert(7,8)
+# print(list)
+
+# list = [6,1,2,3,5,4]
+# list.remove(3)
+# print(list)
+
+# list = [6,1,2,3,5,4]
+# list.pop(3)
+# print(list)
+
+
+
+# tup = (2, 1, 3, 4)
+# print(tup)
+# print(type(tup))
+
+# tup = (2, 1, 3, 4)
+# print(tup.index(1))
+
+# tup = (2, 1, 3, 4)
+# print(tup.count(2))
