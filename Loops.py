3e1e8f8639591a4e1fa499cd453476e2bc4aef91
Muqutadir-Zz <skmuqutadir26@gmail.com>
2026-01-03 23:15:44 +0530

    commit

---
+++ Loops.py
@@ -0,0 +1,115 @@
+# while True :
+#   print("muqutadir")
+
+# count = 1
+# while count <= 5 :
+#     print("hello")
+#     count += 1
+
+# i = 1
+# while i <= 100 :
+#     print("LUFFY")
+#     i+=1
+
+# i = 5 
+# while i>=1 :
+#     print(i)
+#     i -=1
+
+#     print("ZORO")
+
+# PRACTICE
+
+# i = 1
+# while  i<= 100 :
+#     print(i)
+#     i += 1
+
+# i = 100
+# while i>=1 :
+#     print(i)
+#     i -= 1
+
+
+# i = 1
+# while i <= 10 :
+#     print(3*i) 
+#     i += 1  
+
+# i = 1
+# while i <= 10 :
+#     print(99*i) 
+#     i += 1 
+
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# print(nums[0])
+# print(nums[1])
+# print(nums[2])
+# print(nums[3])  #.... print(nums[len(nums)-1])
+
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# idx = 0
+# while idx < len(nums) :
+#     print(nums[idx])
+#     idx += 1
+
+
+# nums =  [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# x = 25
+
+# i = 0
+# while i < len(nums) :
+#     if(nums[i] == x):
+#         print("FOUND at idx" , i)
+#     else : 
+#         print("finding.........")
+#     i += 1
+    
+# i = 1
+# while i <= 5 :
+#     print(i)
+#     if(i == 3) :
+#         break
+#     i += 1
+
+# veggies = ["POTATO", "TOMATO", "LADYFINGER", "CUCUMBER"]
+
+# for val in veggies:
+#     print(val)   # also for tup , also str
+
+# str = "MARVEL"
+
+# for char in str:
+#     if(char == 'R'):
+#         print("R FOUND")
+#         break
+#     print(char)
+    
+#  RANGE() FUNCTION IMP FOR LOOPS
+
+# 1. range(stop)
+
+# seq =range(5)
+
+# for i in range(10) :
+#     print(i)
+
+# 2. range(start, stop)
+
+# for i in range(2, 10) :
+#     print(i)
+
+# 3. range (start, stop, step)
+
+# for i in range(2, 10, 2) :
+#     print(i)
+
+
+# PASS STATEMENT 
+
+# for i in range(3):
+#     pass
+#     print("some usefull work")
