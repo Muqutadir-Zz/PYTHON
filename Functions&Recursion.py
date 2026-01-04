2c8623c37af5d7d2b45e2d69017fb83aac2fb390
Muqutadir-Zz <skmuqutadir26@gmail.com>
2026-01-04 21:40:50 +0530

    commit

---
+++ Functions&Recursion.py
@@ -0,0 +1,23 @@
+#function defination
+# def cal_sum(a,b) : #parameters
+#     return(a,b)
+
+# sum = cal_sum(1,2)   #function call;arguments
+# print (sum)
+
+# def calc_avg(a, b, c) :
+#     sum = a+b+c
+#     avg = sum/3
+#     print(avg)
+#     return avg
+
+# calc_avg(60, 30, 70)
+
+# BUILT IN FUNCTION
+
+#  print()
+
+#  len()  defiine interger value
+
+# tpye()  , range()
+
--- Loops.py
+++ Loops.py
@@ -113,3 +113,10 @@
 # for i in range(3):
 #     pass
 #     print("some usefull work")
+
+n = 5
+sum = 0
+for i in range(1, n+1) :
+    sum += i
+
+    print("total sum", sum)
