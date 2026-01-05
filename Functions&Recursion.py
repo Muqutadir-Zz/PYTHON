ff27a0ca53234d4b8256dfb197675ec5428240ad
Muqutadir-Zz <skmuqutadir26@gmail.com>
2026-01-06 03:33:33 +0530

    commit

--- Functions&Recursion.py
+++ Functions&Recursion.py
@@ -45,3 +45,46 @@
 # conveter(73)
 
 # RECURSION
+
+# def show(n):
+#     if(n == 5):
+#         return
+#     print(n)
+#     show(n-1)
+
+# print(2)
+
+# def show(n):
+#     if(n==0):
+#         return
+#     print(n)
+#     show(n-1)
+
+# show(2) # prints n to 1 backwards
+
+# def fact(n):
+#     if(n==0 or n==1):
+#         return 1
+#     else:
+#         return n * fact(n-1) 
+    
+# print(fact(6))
+
+# PRACTICE
+# def cal_sum(n):
+#     if(n==0):
+#         return 0
+#     return cal_sum(n-1) + n
+
+# sum = cal_sum(55)
+# print(sum)
+
+
+def print_list(list, idx=0):
+    if(idx == len(list)):
+        return
+    print(list[idx])
+    print_list(list, idx+1)
+
+fruits = ["apple", "banana" ]
+print_list(fruits)
